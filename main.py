4173909b70586c3ed941d12505afc6da9bafeb5d
Vonzpf <eternityzpf@sina.com>
2018-01-19 21:08:24 +0800

    add solutions

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 # @Author zpf
 
 from sklearn import linear_model
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import KFold
+from decimal import *
 import csv
 import numpy as np
+import xgboost as xgb
 
 
 def data_process():
@@ -52,24 +58,57 @@ def data_process():
 def predict(train, test):
     train = train.astype(np.float)
     test = test.astype(np.float)
-    
+
     train_normalized = preprocessing.normalize(train[:, :-1], norm='l2')
     test_normalized = preprocessing.normalize(test, norm='l2')
 
-    X = train_normalized[:, :-1]
     Y = train[:, -1]
-    train_x = X[:4000, :]
-    train_y = Y[:4000]
-    val_x = X[4000:, :]
-    val_y = Y[4000:]
 
-    clr = linear_model.LinearRegression()
-    clr.fit(train_x, train_y)
-    y_predict = clr.predict(val_x)
-    print(y_predict)
-    print(val_y)
-    loss = sum((y_predict - val_y) ** 2)/(2 * len(y_predict))
-    print(loss)
+    test_avg_predict = np.zeros(len(test_normalized))
+    test_predict_num = 0
+
+    # 交叉验证
+    kf = KFold(n_splits=20)
+    for train_index, val_index in kf.split(train_normalized):
+        x_train, x_val = train_normalized[train_index], train_normalized[val_index]
+        y_train, y_val = Y[train_index], Y[val_index]
+
+        clr = linear_model.LinearRegression()
+        gbr = GradientBoostingRegressor(n_estimators=20, learning_rate=0.1,
+                                        max_depth=3)
+        clr.fit(x_train, y_train)
+        gbr.fit(x_train, y_train)
+
+        y_predict = clr.predict(x_val)
+        y_predict_gbr = gbr.predict(x_val)
+
+        # print(y_predict)
+        # print(y_val)
+        val_loss = sum((y_predict - y_val) ** 2) / (2 * len(y_predict))
+        val_loss_gbr = sum((y_predict_gbr - y_val) ** 2) / (2 * len(y_predict_gbr))
+        print(str(val_loss) + '\t' + str(val_loss_gbr))
+        print(train_index, val_index)
+        if val_loss < 0.7:
+            test_predict = clr.predict(test_normalized)
+            test_avg_predict += test_predict
+            test_predict_num += 1
+
+    test_avg_predict = test_avg_predict/test_predict_num
+    test_result_list = test_avg_predict.tolist()
+    # print(test_result_list)
+    final_result = []
+    getcontext().prec = 4
+    for e in test_result_list:
+        temp_result = Decimal(e) * Decimal(1)
+        final_result.append(temp_result)
+    # print(final_result)
+
+    csv_file = open('./result.csv', 'w', newline='')
+    writer = csv.writer(csv_file)
+    # for i in range(len(final_result)):
+    final_result = map(lambda x: [x], final_result)
+    writer.writerows(final_result)
+    csv_file.close()
 
 
 if __name__ == "__main__":
